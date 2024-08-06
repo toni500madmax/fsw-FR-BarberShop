8ed91d952f02c2994afcff1a3654354d8fdadc30
toni500madmax <toni500madmax@gmail.com>
2024-08-06 18:17:27 -0300

    feat: add pattern images in next.config.msj

--- barber-system/next.config.mjs
+++ barber-system/next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: {
-            hostname: "utfs.io"
-        }
+        remotePatterns: [{
+            hostname: 'utfs.io'
+        }]
     }
 };
 
